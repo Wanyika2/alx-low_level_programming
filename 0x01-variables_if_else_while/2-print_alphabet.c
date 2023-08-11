e1b5cea0018aa85f88e08bd530055c1ac2ab852f
Wanyika2 <dorothywanyika4@gmail.com>
2023-08-11 05:12:51 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
+
