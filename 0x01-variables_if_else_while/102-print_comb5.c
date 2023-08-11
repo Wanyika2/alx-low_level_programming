0ca3ed78a19c568bab97cc6f23c76262532ac68d
Wanyika2 <dorothywanyika4@gmail.com>
2023-08-11 14:58:42 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
@@ -23,12 +21,11 @@ int main(void)
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
-		continue;
+	continue;
 	putchar(',');
 	putchar(' ');
 	}
 	}
 	putchar('\n');
-
 	return (0);
 }
