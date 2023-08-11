f6143437f07d37086b0cbd8ee25288f69e8e2d05
Wanyika2 <dorothywanyika4@gmail.com>
2023-08-11 15:52:31 -0700

    Do what you do better and do more o what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -8,10 +10,9 @@
 int main(void)
 
 {
-
 	int d, p, q;
 
-	for (d = '0'; d <=  '9'; d++)
+	for (d = '0'; d < '9'; d++)
 	{
 
 	for (p = d + 1; p <= '9'; p++)
@@ -19,7 +20,6 @@ int main(void)
 
 	for (q = p + 1; q <= '9'; q++)
 	{
-
 	if ((p != d) != q)
 	{
 	putchar(d);
@@ -27,7 +27,7 @@ int main(void)
 	putchar(q);
 	if (d == '7' && p == '8')
 	continue;
-	putchar('.');
+	putchar(',');
 	putchar(' ');
 	}
 	}
