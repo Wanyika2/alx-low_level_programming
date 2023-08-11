f4a0db5a02c3dafc6997e03433ca52dd5b54fabc
Wanyika2 <dorothywanyika4@gmail.com>
2023-08-11 14:22:21 -0700

    Do what you do better and do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 
@@ -15,7 +12,7 @@ int main(void)
 {
 	int d, p, q;
 
-	for (d = '0'; d <= '9'; d++)
+	for (d = '0'; d < '9'; d++)
 	{
 	for (p = d + 1; p <= '9'; p++)
 	{
