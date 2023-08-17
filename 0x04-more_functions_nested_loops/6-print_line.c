5866cc52a222f28b67f6b77c576eff696a27960e
Wanyika2 <dorothywanyika4@gmail.com>
2023-08-17 10:02:26 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,19 @@
 void print_line(int n)
 
 {
+
 	int x;
 
 	if (n <= 0)
+
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for(x = 1; x < n; x++);
-		{
-			_putchar(95);
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
 		}
 		_putchar('\n');
 	}
